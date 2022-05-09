d1645a44ef6e19e4bc1787cac5b4c15063802960
Richard Demeny <richarddemeny@Richards-MacBook-Pro.local>
2022-05-09 01:50:59 +0100

    test

--- app.test.js
+++ app.test.js
@@ -3,15 +3,34 @@ const app = require('./app');
 const axios = require('axios');
 const MockAdapter = require('axios-mock-adapter');
 
-const mock = new MockAdapter(axios);
+const mockAxios = new MockAdapter(axios);
 
 describe('Test branch microservice', () => {
   test('GET /branch should return 200 with branch info if branch is found', async () => {
+    const response = await request(app)
+      .get('/branch')
+      .set('lbg-txn-branch-location', 'london');
+    expect(response.statusCode).toBe(200);
   });
   test('GET /branch should return 404 if branch is not found', async () => {
+    const response = await request(app)
+      .get('/branch')
+      .set('lbg-txn-branch-location', 'kidderminster');
+    expect(response.statusCode).toBe(404);
   });
   test('GET /branch should return 400 if malformed or no header is received', async () => {
+    const response = await request(app)
+      .get('/branch')
+      .set('lbg-txn-branch-locatio', 'london');
+    expect(response.statusCode).toBe(400);
+    const response2 = await request(app).get('/branch');
+    expect(response2.statusCode).toBe(400);
   });
   test('Using axios-mock', async () => {
+    mockAxios
+      .onGet(`localhost:3000/branch`, undefined, {
+        'lbg-txn-branch-location': 'london',
+      })
+      .reply(200);
   });
 });
