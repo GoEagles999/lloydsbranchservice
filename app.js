c51b41c65f64ba6e08ded22d7c64144e4e121b53
Richard Demeny <richarddemeny@Richards-MacBook-Pro.local>
2022-07-14 13:55:30 +0100

    asd

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const getBranch = require('./handlers/getBranch');
 const app = express();
 
-app.get('/branch', getBranch);
+app.get('/branch', getBraanch);
 
 module.exports = app;
